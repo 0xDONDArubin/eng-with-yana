f93ccb0c01baca582cf23c258ae1eef51b56b6f5
0xDONDArubin <102237672+0xDONDArubin@users.noreply.github.com>
2022-10-24 17:46:04 +0400

    Update AboutMePage.jsx

--- src/pages/AboutMePage/AboutMePage.jsx
+++ src/pages/AboutMePage/AboutMePage.jsx
@@ -13,24 +13,14 @@ const AboutMePage = () => {
           <div className="about-me_container2">
             <div className="about-me_text">
               <p>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit,
-                sed do eiusmod tempor incididunt ut labore et dolore magna
-                aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
-                laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor
-                in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
-                Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit,
-                sed do eiusmod tempor incididunt ut labore et dolore magna
-                aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
-                laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor
-                in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
-                Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit,
-                sed do eiusmod tempor incididunt ut labore et dolore magna
-                aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
-                laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor
-                in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
-                Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                Меня зовут Яна. Я являюсь студенткой факультета иностранных языков и лингводидактики. Все педагогические и лингвистические дисциплины освоены мной на «отлично». Мой педагогический путь начался 2 года назад, хотя преподавание увлекло меня гораздо раньше. Я являюсь педагогом в 3 поколении. Свою жизнь я непременно хочу связать с методикой преподавания. Не так давно вдохновилась написанием статей на методические и лингвистические темы, что стало поводом публикации в научном журнале.
+                С моей первой статьей, которая поступила в печать, вы можете ознакомиться на сайте.
+
+                На моем счету 1632 часа проведённых занятий. На уроках я совмещаю коммуникативный и классический подход. Занятия провожу на английском языке, переходя на русский язык лишь в крайней необходимости.
+                Внедряю элементы геймификации и отталкиваюсь от интересов учеников. Самостоятельно разрабатываю игры на онлайн платформах. Помимо лингвистического аспекта иностранного языка, учитываю и экстралингвистический аспект для полного погружения в мир изучаемой дисциплины.
+                На уроках с малышами и младшими школьниками использую предметно-языковое интегрированное обучение.
+                Работаю с разными возрастами и уровнями знания английского языка. Моему самому младшему ученику 4 года, а самому старшему 63.
+                Для каждого ученика разрабатываю индивидуальную программу, которая основывается на поставленной цели, типе модальности. Занятия провожу в оффлайн и онлайн форматах.
               </p>  
             </div>
             <div className="about-me_img">
